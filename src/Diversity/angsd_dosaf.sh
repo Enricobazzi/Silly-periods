5a4327d47bfd08791285e748c40e119c30a9fdaa
Enrico Bazzicalupo <enricobazzical@gmail.com>
2026-09-23 11:20:21 +0200

    less threads and -ref added

--- src/Diversity/angsd_dosaf.sh
+++ src/Diversity/angsd_dosaf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH -A naiss2025-5-565
-#SBATCH -p shared
+#SBATCH -p main
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH -t 0-24:00:00
 #SBATCH --mem=64G
 
@@ -10,7 +10,7 @@
 ml angsd
 
 # constants
-THREADS=16
+THREADS=8
 REF=Reference/GCF_900700415.2_Ch_v2.0.2_genomic.fna
 OUT=data/diversity
 
@@ -25,7 +25,7 @@ minD=$(( n_ind * 2 ))
 maxD=$(( n_ind * 8 ))
 
 # run angsd dosaf
-angsd -bam ${bamlist} -doSaf 1 -anc ${REF} -GL 1 -P ${THREADS} \
+angsd -bam ${bamlist} -doSaf 1 -ref ${REF} -anc ${REF} -GL 1 -P ${THREADS} \
     -out ${OUT}/${dataset} \
     -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -baq 1 -C 50 \
     -minMapQ 30 -minQ 30 -doCounts 1 -noTrans 1 \
